e6afd3a5b418eb63fe68ce3e23eb3b1623a55309
Lucas Maurice <lucas.maurice@outlook.com>
2018-07-10 12:21:36 -0400

    Make a script for generate galery

--- img/galery/generate.sh
+++ img/galery/generate.sh
@@ -1,9 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
 echo Will generate a galery
 
-for filename in /Data/*.txt; do
-    for ((i=0; i<=3; i++)); do
-        echo $filename is cool
-    done
+FILES=./brut/*.jpg
+NAME_ID=$((001))
+
+echo    '<juiceboxgallery galleryTitle="">' > ./config.xml
+
+for f in $FILES
+do
+    NAME_IMG=./images/img_$(printf "%02d\n" $NAME_ID).jpg
+    NAME_THUMB=./images/img_thumb_$(printf "%02d\n" $NAME_ID).jpg
+
+    echo "Processing $f file to $NAME_IMG and $NAME_THUMB ... "
+
+    cp "$f" $NAME_IMG
+
+    convert $NAME_IMG -resize 85x85 $NAME_THUMB
+
+    
+    echo    "    <image imageURL=\"img/galery/images/$NAME_IMG\"" >> ./config.xml
+	echo    "        thumbURL=\"img/galery/images/$NAME_THUMB\"" >> ./config.xml
+	echo    "        linkURL=\"img/galery/images/$NAME_IMG\"" >> ./config.xml
+	echo    "        linkTarget=\"_blank\">" >> ./config.xml
+	echo    "        <title></title>" >> ./config.xml
+    echo    "    </image>" >> ./config.xml
+
+
+
+
+
+
+
+  NAME_ID=$((NAME_ID+1))
+  # take action on each file. $f store current file name
 done
+
+echo    "</juiceboxgallery>" >> ./config.xml
